3a614726523e02f9e0570b36f0418b68d6c363ff
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-19 02:09:45 +0900

    [add] header component

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BaseUser from '@/components/BaseUser.vue'
+</script>
+
+<template>
+  <div class="w-full h-20 flex items-center justify-end px-10">
+    <BaseUser user-icon-url="" user-name="test" />
+  </div>
+</template>
