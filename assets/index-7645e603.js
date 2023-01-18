000258c0c77c8ba8c5c3ba082ab45e7ebf774207
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2023-01-18 17:10:22 +0000

    Deploying to gh-pages from  @ 3a614726523e02f9e0570b36f0418b68d6c363ff 🚀

--- assets/index-7645e603.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as a,c as o,b as e,t as r}from"./index-70be3e42.js";const c={},n=e("div",{class:"text-red-500"},"slide",-1);function d(s,i){return a(),o("div",null,[n,e("div",null,r(s.$route.params.id),1)])}const _=t(c,[["render",d]]);export{_ as default};
--- assets/share-abb4fa55.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as r,c as t,b as e,t as o}from"./index-70be3e42.js";const c={},n=e("div",{class:"text-red-500"},"share",-1);function _(s,d){return r(),t("div",null,[n,e("div",null,o(s.$route.params.share_id),1)])}const l=a(c,[["render",_]]);export{l as default};
