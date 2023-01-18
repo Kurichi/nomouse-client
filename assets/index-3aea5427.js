cdbe34416f01a71370bc92e520d41a3060ee25bf
Simo-C3 <65708479+Simo-C3@users.noreply.github.com>
2023-01-18 16:32:40 +0000

    Deploying to gh-pages from  @ 333f6a71a89cc103e45da0c3e8b9995355f83bc8 🚀

---
+++ assets/index-3aea5427.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-70be3e42.js";const n={},o={class:"text-red-500"};function s(r,_){return c(),t("div",o,"index")}const d=e(n,[["render",s]]);export{d as default};
