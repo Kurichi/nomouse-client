5142ae9fad1c2578665f86650d2366c4515d4c8d
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-19 21:14:23 +0900

    [add] page/slide/[id].vue

---
+++ pages/slide/share/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="text-red-500">share</div>
+    <div>{{ $route.params.share_id }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
