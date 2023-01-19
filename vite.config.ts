4562ae7fcc78e61d7b5c5ee09400a2dc4458ffbe
Kurichi <yuuyapp080@gmail.com>
2023-01-19 17:29:22 +0900

    [fix] GitHub Pages 404エラーの解消
    ビルド時オプションにsanitizeFileNameを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,33 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+import { sanitizeFileName } from "./vite/util";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.join(__dirname, 'src'),
+      "@": path.join(__dirname, "src"),
     },
   },
   server: {
     host: true,
     port: 3000,
   },
-  base: process.env.NODE_ENV === 'production' ? '/nomouse-client/' : './',
+  base: process.env.NODE_ENV === "production" ? "/nomouse-client/" : "./",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            return id.split("/node_modules/").pop()?.split("/")[0];
+          }
+        },
+        sanitizeFileName,
+      },
+    },
   },
-})
+});
