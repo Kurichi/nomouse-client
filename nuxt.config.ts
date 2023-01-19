eac9257d8e220156e54c9c323760b5100dd0d4c4
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-19 21:11:17 +0900

    [add] tailwindcss/sass/google icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,23 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'My App',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'My amazing site.' },
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+        },
+      ],
+    },
     baseURL: '/nomouse-client',
   },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['@/assets/css/tailwind.css'],
 })
