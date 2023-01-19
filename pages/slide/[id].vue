11689cb04b28d88415fbbb3dbb9c2c374c18e92e
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-19 21:13:55 +0900

    [add] page/[id].vue

---
+++ pages/slide/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div class="text-red-500">slide</div>
+    <div>{{ $route.params.id }}</div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
