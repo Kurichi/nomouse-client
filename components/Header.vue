80a7eb87a610b677db84915cd602c3e1ee17f613
Kurichi <yuuyapp080@gmail.com>
2023-01-20 22:44:20 +0900

    [update] アイコン画像
    ログイン時にアイコン画像が表示されるようにした

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import BaseUser from '@/components/BaseUser.vue'
+import BaseUser from "@/components/BaseUser.vue";
+import { getAuth } from "@firebase/auth";
+
+const auth = getAuth();
+const avatar = auth.currentUser?.photoURL;
 </script>
 
 <template>
   <div class="w-full h-[var(--header-h)] flex items-center justify-end px-10">
-    <BaseUser user-icon-url="" user-name="test" />
+    <BaseUser :user-icon-url="avatar ?? ''" user-name="test" />
   </div>
 </template>
