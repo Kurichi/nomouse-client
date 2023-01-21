a2f3bbaf2c9a182382cf759abe923dea640feb06
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-21 21:20:05 +0900

    [add] top page デザイン

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-import BaseUser from "@/components/BaseUser.vue";
-import { getAuth } from "@firebase/auth";
+import BaseUser from '@/components/BaseUser.vue'
+import { getAuth } from '@firebase/auth'
 
-const auth = getAuth();
-const avatar = auth.currentUser?.photoURL;
+const auth = getAuth()
+const avatar = auth.currentUser?.photoURL
 </script>
 
 <template>
-  <div class="w-full h-[var(--header-h)] flex items-center justify-end px-10">
+  <div
+    class="w-full h-[var(--header-h)] flex items-center justify-end px-10 bg-slate-100"
+  >
     <BaseUser :user-icon-url="avatar ?? ''" user-name="test" />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>hoge</div>
+  <div
+    class="w-full h-[var(--top-h)] bg-slate-100 relative"
+    style="--top-h: calc(100vh - var(--header-h))"
+  >
+    <img
+      src="@/assets/image/top1.png"
+      alt="top1"
+      class="absolute top-[15%] left-[10%] rounded-2xl shadow-xl shadow-gray-400 min-w-[300px] w-[40%] h-auto"
+    />
+    <img
+      src="@/assets/image/top1.png"
+      alt="top1"
+      class="absolute top-[25%] left-[20%] rounded-2xl shadow-xl shadow-gray-600 min-w-[300px] w-[40%] h-auto"
+    />
+    <router-link
+      to="/login"
+      class="rounded-lg shadow-lg bg-slate-800 text-white cursor-pointer px-5 py-3 absolute left-1/2 -translate-x-1/2 top-[70%]"
+    >
+      はじめる
+    </router-link>
+  </div>
 </template>
