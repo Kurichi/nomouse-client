3f0ec778e21115be9f937c6898256400753c704c
Kurichi <yuuyapp080@gmail.com>
2023-01-21 20:17:44 +0900

    [update] KeyBoard Input

--- components/BaseSlide.vue
+++ components/BaseSlide.vue
@@ -3,6 +3,7 @@ const props = defineProps({
   width: { type: Number, default: 350 },
   shadowSize: { type: String, default: 'base' },
   slideData: { type: Object, required: true },
+  slideShow: { type: Boolean, default: false },
 });
 
 const shadowSizeHandler = (): string => {
@@ -25,8 +26,15 @@ const shadowSizeHandler = (): string => {
 
 <template>
   <div
-    class="shadow-gray-300 bg-white w-[var(--slide-w)] h-[var(--slide-h)] overflow-hidden rounded"
-    :class="shadowSizeHandler()"
+    class="shadow-gray-300 bg-white overflow-hidden rounded"
+    :class="[
+      shadowSizeHandler(),
+      {
+        'w-screen h-screen absolute -top-[var(--header-h)] left-0 z-50':
+          slideShow,
+      },
+      { 'w-[var(--slide-w)] h-[var(--slide-h)]': !slideShow },
+    ]"
     :style="`--slide-w: ${props.width}px; --slide-h: ${props.width * 0.6}px;`"
   >
     <!-- <canvas class="w-[var(--slide-w)] h-[var(--slide-h)]" /> -->
---
+++ components/Presentation.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const currentPage = ref(0);
+
+const props = defineProps({
+  slides: { type: Object, required: true }, // v-model用
+});
+
+onMounted(() => {
+  addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight' && currentPage.value + 1 < props.slides.length) {
+      currentPage.value++;
+    } else if (e.key === 'ArrowLeft' && currentPage.value > 0)
+      currentPage.value--;
+  });
+});
+</script>
+
+<template>
+  <BaseSlide :slide-data="props.slides[currentPage]" :slide-show="true" />
+</template>
