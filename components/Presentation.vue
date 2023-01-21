a750853e62b78bfc147d00f0dd1f46382449f87f
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-22 08:42:36 +0900

    [change] フルスクリーン中の挙動とフルスクリーン終了実装

--- components/Presentation.vue
+++ components/Presentation.vue
@@ -8,21 +8,40 @@ const props = defineProps({
 
 onMounted(() => {
   addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowRight' && currentPage.value + 1 < props.slides.length) {
-      currentPage.value++
+    if (e.key === 'ArrowRight' || e.key === 'Enter') {
+      if (currentPage.value + 1 < props.slides.length) {
+        currentPage.value++
+      } else {
+        exitFullscreen()
+      }
     } else if (e.key === 'ArrowLeft' && currentPage.value > 0)
       currentPage.value--
   })
 })
+
+const exitFullscreen = () => {
+  currentPage.value = 0
+  document.exitFullscreen()
+}
+
+const nextPage = () => {
+  if (currentPage.value + 1 < props.slides.length) {
+    currentPage.value++
+  } else {
+    exitFullscreen()
+  }
+}
 </script>
 
 <template>
   <div
     class="w-screen h-screen absolute -top-[var(--header-h)] left-0 z-40 bg-black"
+    @click="nextPage()"
   >
     <BaseSlide
-      :slide-data="props.slides[currentPage]"
+      :slide-data="props.slides[currentPage].elements"
       :width="screenWidth"
+      :slide-index="currentPage"
       slide-id="presentation"
       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50"
     />
