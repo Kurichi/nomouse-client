2ce98f01124365b375e79fac8d1f145d8b0a2de4
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-01-22 14:49:07 +0900

    [change] デザイン調整

--- pages/index.vue
+++ pages/index.vue
@@ -6,15 +6,19 @@
     style="--top-h: calc(100vh - var(--header-h))"
   >
     <img
-      src="@/assets/image/top1.png"
+      src="https://cdn.discordapp.com/attachments/703125436053454932/1066566330309951609/IMB_BUduLD.GIF"
       alt="top1"
-      class="absolute top-[15%] left-[10%] rounded-2xl shadow-xl shadow-gray-400 min-w-[300px] w-[40%] h-auto"
+      class="absolute top-[15%] left-[15%] rounded-2xl shadow-xl shadow-gray-600 min-w-[500px] w-[40%] h-auto"
     />
-    <img
-      src="@/assets/image/top1.png"
-      alt="top1"
-      class="absolute top-[25%] left-[20%] rounded-2xl shadow-xl shadow-gray-600 min-w-[300px] w-[40%] h-auto"
-    />
+    <div
+      class="absolute top-[40%] left-[50%] rounded-2xl bg-white bg-opacity-60 w-[500px] h-[200px]"
+    >
+      <div
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[80%] text-[30px] text-gray-800"
+      >
+        Markupでスライドを作ろう！
+      </div>
+    </div>
     <router-link
       to="/me"
       class="rounded-lg shadow-lg bg-slate-800 text-white cursor-pointer px-5 py-3 absolute left-1/2 -translate-x-1/2 top-[70%]"
